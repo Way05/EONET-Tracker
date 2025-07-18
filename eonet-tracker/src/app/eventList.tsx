34d15bb4079c5f4368a0a5145a4d7af65887e33b
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-17 20:54:30 -0400

    renders basic event info

--- eonet-tracker/src/app/eventList.tsx
+++ eonet-tracker/src/app/eventList.tsx
@@ -1,19 +1,56 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface eventFormat {
+  id: string;
+  title: string;
+  description: string;
+  link: string;
+  closed: boolean;
+  //LAZY I WILL ADD THEM IF I NEED THEM
+  categories: any;
+  sources: any;
+  geometry: any;
+}
+
+interface allEventsFormat {
+  title: string;
+  description: string;
+  link: string;
+  events: eventFormat[];
+}
+
 export default function EventList() {
-  //   const api_key: string = process.env.TOKEN!;
-  const params = new URLSearchParams();
+  const eventLimit: number = 20;
+  const categoryFilter: string = "";
+  const [currData, setData] = useState<allEventsFormat>();
+  const params: URLSearchParams = new URLSearchParams();
   params.append("status", "open");
-  params.append("limit", "1");
-  //   params.append("api_key", api_key);
-  async function getData() {
-    const res = await fetch(
-      `https://eonet.gsfc.nasa.gov/api/v3/events?${params}`,
-      {
-        next: { revalidate: 3600 },
-      },
-    );
-    const json = await res;
-    console.log(json);
-  }
-  getData();
-  return <div></div>;
+  params.append("limit", eventLimit.toString());
+
+  useEffect(() => {
+    async function getData(): Promise<void> {
+      const res: Response = await fetch(
+        `https://eonet.gsfc.nasa.gov/api/v3/events?${params}`,
+        {
+          next: { revalidate: 3600 },
+        },
+      );
+      const json: allEventsFormat = await res.json();
+      setData(json);
+
+      console.log(json);
+    }
+
+    getData();
+  }, []);
+  return (
+    <div>
+      {currData
+        ? currData.events.map((event: eventFormat) => (
+            <div key={event.id}>{event.title}</div>
+          ))
+        : "Loading..."}
+    </div>
+  );
 }
