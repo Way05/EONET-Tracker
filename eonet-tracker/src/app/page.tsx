9fcdb69a8cf268d4adb825ad3cd5f0fa9036c969
Wei-en Tan <wtan235@gmail.com>
2025-07-19 13:48:48 -0400

    added empty dep to prevent constant rerender

--- eonet-tracker/src/app/page.tsx
+++ eonet-tracker/src/app/page.tsx
@@ -27,13 +27,13 @@ export default function Home() {
     }
 
     // getData();
-  });
+  }, []);
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-wrap items-center gap-[32px] sm:items-start">
         <EventList events={currData?.events}></EventList>
         <Map></Map>
-        {/* <Graph events={currData?.events}></Graph> */}
+        <Graph events={currData?.events}></Graph>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
