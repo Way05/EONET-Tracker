fe7757e4ee7bd3b18cebe24774846eb9f82a95a7
Wei-en Tan <wtan235@gmail.com>
2025-07-19 14:06:36 -0400

    properly formats data into graph

--- eonet-tracker/src/app/components/graph.tsx
+++ eonet-tracker/src/app/components/graph.tsx
@@ -16,15 +16,8 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/src/app/components/ui/chart";
-
-const chartData = [
-  { month: "January", events: 186 },
-  { month: "February", events: 305 },
-  { month: "March", events: 237 },
-  { month: "April", events: 73 },
-  { month: "May", events: 209 },
-  { month: "June", events: 214 },
-];
+import { eventFormat } from "./dataInterfaces";
+import { useEffect, useState } from "react";
 
 const chartConfig = {
   events: {
@@ -33,20 +26,58 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export default function Graph() {
+type data = {
+  events: eventFormat[] | undefined;
+};
+
+type chartEntry = {
+  date: string;
+  events: number;
+};
+
+interface tempObjData {
+  [key: string]: number;
+}
+
+export default function Graph(props: data) {
+  const [chartData, setChartData] = useState<chartEntry[]>([]);
+  const [totalEvents, setTotalEvents] = useState<number>(0);
+
+  useEffect(() => {
+    if (props.events) {
+      setTotalEvents(props.events?.length);
+      const tempChartData: tempObjData = {};
+      for (const event of props.events) {
+        const date: string = event.geometry[0].date.slice(0, 10);
+        if (!tempChartData[date]) {
+          tempChartData[date] = 1;
+        } else {
+          tempChartData[date]++;
+        }
+      }
+
+      const formattedData: chartEntry[] = Object.keys(tempChartData).map(
+        (key) => ({ date: key, events: tempChartData[key] }),
+      );
+      setChartData(formattedData.reverse());
+
+      // console.log(tempChartData);
+      // console.log(chartData);
+    }
+  }, [props.events]);
   return (
     <div>
       <Card>
         <CardHeader className="flex flex-col items-stretch border-b p-0 sm:flex-row">
           <div className="flex flex-1 flex-col justify-center gap-1 px-6">
             <CardTitle>Event Frequency</CardTitle>
-            <CardDescription>Events per Day</CardDescription>
+            <CardDescription>Ongoing Events per Day</CardDescription>
           </div>
           <div className="flex">
             <div className="border-- relative z-30 flex flex-1 flex-col justify-center gap-1 px-6 text-left even:border-l sm:border-l sm:border-t-0 sm:px-8">
               <span className="text-muted-foreground text-xs">Total</span>
               <span className="text-lg font-bold leading-none sm:text-2xl">
-                100
+                {totalEvents}
               </span>
             </div>
           </div>
@@ -57,15 +88,32 @@ export default function Graph() {
             <BarChart accessibilityLayer data={chartData}>
               <CartesianGrid vertical={false} />
               <XAxis
-                dataKey="month"
+                dataKey="date"
                 tickLine={false}
                 tickMargin={10}
                 axisLine={false}
-                tickFormatter={(value) => value.slice(0, 3)}
+                minTickGap={32}
+                tickFormatter={(value) => {
+                  const date = new Date(value);
+                  return date.toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                  });
+                }}
               />
               <ChartTooltip
                 cursor={false}
-                content={<ChartTooltipContent hideLabel />}
+                content={
+                  <ChartTooltipContent
+                    labelFormatter={(value) => {
+                      return new Date(value).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                      });
+                    }}
+                  />
+                }
               />
               <Bar dataKey="events" fill="var(--color-events)" radius={3} />
             </BarChart>
@@ -73,10 +121,10 @@ export default function Graph() {
         </CardContent>
         <CardFooter className="flex-col items-start gap-2 text-sm">
           <div className="flex gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            No trend data.
           </div>
           <div className="text-muted-foreground leading-none">
-            Showing total visitors for the last 6 months
+            Showing the last {totalEvents} disasters.
           </div>
         </CardFooter>
       </Card>
