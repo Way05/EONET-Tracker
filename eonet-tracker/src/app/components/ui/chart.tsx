ff9c2ffc070becbca1c584a8bc675bcef74d50ab
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-18 22:17:34 -0400

    removed dupe styles

--- eonet-tracker/src/app/components/ui/chart.tsx
+++ eonet-tracker/src/app/components/ui/chart.tsx
@@ -171,7 +171,7 @@ function ChartTooltipContent({
   return (
     <div
       className={cn(
-        "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-neutral-200 border-neutral-200/50 bg-white px-2.5 py-1.5 text-xs shadow-xl dark:border-neutral-800 dark:border-neutral-800/50 dark:bg-neutral-950",
+        "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-neutral-200 bg-white px-2.5 py-1.5 text-xs shadow-xl dark:border-neutral-800 dark:bg-neutral-950",
         className,
       )}
     >
