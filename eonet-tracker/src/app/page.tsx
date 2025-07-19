495cdc8ebc3cc4142b8a579c8530bb82b40735d1
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-18 20:36:54 -0400

    setup map

--- eonet-tracker/src/app/page.tsx
+++ eonet-tracker/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import EventList from "./eventList";
+import Map from "../map";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+      <main className="row-start-2 flex flex-wrap items-center gap-[32px] sm:items-start">
         <EventList></EventList>
+        <Map></Map>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
