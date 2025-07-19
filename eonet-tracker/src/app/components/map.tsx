bed172e87f9b290b4b5d4fe57944554a0fca66a7
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-19 19:40:53 -0400

    map added and styled

--- eonet-tracker/src/app/components/map.tsx
+++ eonet-tracker/src/app/components/map.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardContent,
@@ -5,17 +6,33 @@ import {
   CardHeader,
   CardTitle,
 } from "@/src/app/components/ui/card";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
 
 export default function Map() {
   return (
     <div>
-      <Card>
+      <Card className="aspect-square">
         <CardHeader>
           <CardTitle>Event Map</CardTitle>
           <CardDescription>View the Map</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>[MAP HERE]</p>
+          <div id="map" className="aspect-square">
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={false}
+              className="h-100 w-100 rounded-lg"
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </MapContainer>
+          </div>
         </CardContent>
       </Card>
     </div>
