9b655dee1cc5ad910a544ec53dc91484e10c0898
Wei-en Tan <wtan235@gmail.com>
2025-07-19 13:13:30 -0400

    moved api fetching to main

--- eonet-tracker/src/app/page.tsx
+++ eonet-tracker/src/app/page.tsx
@@ -1,15 +1,39 @@
+"use client";
 import Image from "next/image";
 import EventList from "./components/eventList";
 import Map from "./components/map";
 import Graph from "./components/graph";
+import { useEffect, useState } from "react";
+import { allEventsFormat } from "./components/dataInterfaces";
 
 export default function Home() {
+  const eventLimit: number = 20;
+  const [currData, setData] = useState<allEventsFormat>();
+  const params: URLSearchParams = new URLSearchParams();
+  params.append("status", "open");
+  params.append("limit", eventLimit.toString());
+  useEffect(() => {
+    async function getData(): Promise<void> {
+      const res: Response = await fetch(
+        `https://eonet.gsfc.nasa.gov/api/v3/events?${params}`,
+        {
+          next: { revalidate: 3600 },
+        },
+      );
+      const json: allEventsFormat = await res.json();
+      setData(json);
+
+      console.log(json);
+    }
+
+    // getData();
+  });
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-wrap items-center gap-[32px] sm:items-start">
-        <EventList></EventList>
+        <EventList events={currData?.events}></EventList>
         <Map></Map>
-        <Graph></Graph>
+        {/* <Graph events={currData?.events}></Graph> */}
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
