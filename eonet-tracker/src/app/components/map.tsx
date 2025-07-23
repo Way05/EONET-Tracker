8305cf32d8e461b3bbb6b6f180a91cb854804e24
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-22 20:38:02 -0400

    added live coords & reset button

--- eonet-tracker/src/app/components/map.tsx
+++ eonet-tracker/src/app/components/map.tsx
@@ -1,11 +1,14 @@
 "use client";
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/src/app/components/ui/card";
+import { Button } from "./ui/button";
 import {
   MapContainer,
   TileLayer,
@@ -18,11 +21,40 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet-defaulticon-compatibility";
 import { eventFormat, geometryFormat, propsListEvents } from "./dataInterfaces";
-import { JSX, useState } from "react";
+import { JSX, useCallback, useEffect, useMemo, useState } from "react";
+import { Crosshair } from "lucide-react";
+
+const center: [number, number] = [40, -98];
+const zoom: number = 3;
+
+function DisplayPosition({ map }: Map) {
+  const [position, setPosition] = useState(() => map.getCenter());
+
+  const onMove = useCallback(() => {
+    setPosition(map.getCenter());
+  }, [map]);
+
+  useEffect(() => {
+    map.on("move", onMove);
+    return () => {
+      map.off("move", onMove);
+    };
+  }, [map, onMove]);
+
+  return (
+    <div>
+      <p className="text-sm font-medium">
+        Latitude: {position.lat.toFixed(2)}, Longitude:
+        {` ${position.lng.toFixed(2)}`}
+      </p>
+    </div>
+  );
+}
 
 export default function Map(props: propsListEvents) {
+  const [map, setMap] = useState<Map | undefined>(null);
   const [selected, setSelected] = useState<string>();
-  function drawVectorLayer(geoData: geometryFormat[]): JSX.Element {
+  function drawVectorLayer(id: string, geoData: geometryFormat[]): JSX.Element {
     const colorRed = { color: "red" };
     if (geoData.length > 1) {
       const polygon: [number, number][] = geoData.map(
@@ -31,10 +63,13 @@ export default function Map(props: propsListEvents) {
           coordPair.coordinates[0],
         ],
       );
-      return <Polygon pathOptions={colorRed} positions={polygon}></Polygon>;
+      return (
+        <Polygon key={id} pathOptions={colorRed} positions={polygon}></Polygon>
+      );
     } else {
       return (
         <Circle
+          key={id}
           center={[geoData[0].coordinates[1], geoData[0].coordinates[0]]}
           pathOptions={colorRed}
           radius={10000}
@@ -42,46 +77,61 @@ export default function Map(props: propsListEvents) {
       );
     }
   }
+
+  const onClick = useCallback(() => {
+    map.setView(center, zoom);
+  }, [map]);
+  const displayMap = useMemo(
+    () => (
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        className="h-100 w-100 rounded-lg"
+        ref={setMap}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {props.events
+          ? props.events.map((event: eventFormat) => (
+              <Marker
+                key={event.id}
+                position={[
+                  event.geometry[0].coordinates[1],
+                  event.geometry[0].coordinates[0],
+                ]}
+              >
+                <Popup>{event.title}</Popup>
+              </Marker>
+            ))
+          : null}
+      </MapContainer>
+    ),
+    [],
+  );
+
   return (
     <div>
       <Card className="aspect-square">
         <CardHeader>
           <CardTitle>Event Map</CardTitle>
           <CardDescription>View the Map</CardDescription>
+          <CardAction>
+            <Button variant="outline" onClick={onClick} className="h-10 w-10">
+              <Crosshair></Crosshair>
+            </Button>
+          </CardAction>
         </CardHeader>
         <CardContent>
           <div id="map" className="aspect-square">
-            <MapContainer
-              center={[40, -98]}
-              zoom={3}
-              scrollWheelZoom={false}
-              className="h-100 w-100 rounded-lg"
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {props.events
-                ? props.events.map((event: eventFormat) => (
-                    <Marker
-                      key={event.id}
-                      position={[
-                        event.geometry[0].coordinates[1],
-                        event.geometry[0].coordinates[0],
-                      ]}
-                    >
-                      <Popup>{event.title}</Popup>
-                    </Marker>
-                  ))
-                : null}
-              {props.events
-                ? props.events?.map((event: eventFormat) =>
-                    drawVectorLayer(event.geometry),
-                  )
-                : null}
-            </MapContainer>
+            {displayMap}
           </div>
         </CardContent>
+        <CardFooter>
+          {map ? <DisplayPosition map={map}></DisplayPosition> : null}
+        </CardFooter>
       </Card>
     </div>
   );
