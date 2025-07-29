e45694162c5996c638ff435833daada00de0a32c
Wei-en Tan <wtan235@gmail.com>
2025-07-29 18:11:00 -0400

    added link to github

--- eonet-tracker/src/app/page.tsx
+++ eonet-tracker/src/app/page.tsx
@@ -256,6 +256,21 @@ export default async function Home() {
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/Way05/EONET-Tracker"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Github
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://eonet.gsfc.nasa.gov/"
           target="_blank"
           rel="noopener noreferrer"
